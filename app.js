26a5cfe5cd32251dfe0991c7a5f5030f4a1085bc
Konstantin Sharohin <ksharohin@gmail.com>
2019-07-13 22:35:02 +0300

    Added 'typing' note

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
 const app = express();
 const port = 3000;
-const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer);
+const hostname = '127.0.0.1';
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
 
-app.use('/favicon.ico', express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-    console.log('New user connected');
+  console.log('New user connected');
 
-    socket.username = 'Anonymous';
+  socket.username = 'Anonymous';
 
-    socket.on('change username', data => {
-        socket.username = data.username;
-      });
-
-    socket.on('chat message', data => {
-        io.sockets.emit('chat message', {message: data.message, username: socket.username});
-      });
-
-    socket.on('typing', () => {
-      socket.broadcast.emit('typing', {username: socket.username});
-    })
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
+  socket.on('change username', data => {
+    socket.username = data.username;
   });
 
-  httpServer.listen(port, () => {
-    console.log('listening on port: 3000');
+  socket.on('chat message', data => {
+    io.sockets.emit('chat message', {message: data.message, username: socket.username});
+  });
+
+  socket.on('typing', data => {
+    socket.broadcast.emit('notifyTyping', {username: socket.username, message: data.message});
+  });
+
+  socket.on('stopTyping', () => {
+    socket.broadcast.emit('notifyStopTyping');
+  });
+
+  socket.on('disconnect', () => {
+    console.log("User disconnected");
+  });
 });
+
+server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`));
